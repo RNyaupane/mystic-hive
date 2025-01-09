64d3dfbbea3d6c0511cb00d96f9bb57b5decb262
Buddhi Sagar Nyaupane <rabinyaupane55@gmail.com>
2025-01-09 19:15:48 +0545

    New changes

--- dist/assets/main-layout-D4BhHuaq.js
+++
@@ -1,1 +0,0 @@
-import{j as s,O as t}from"./index-CW7ySsWU.js";import{H as r,F as a}from"./header-7bqkIpzA.js";const x=()=>s.jsxs(s.Fragment,{children:[s.jsx(r,{}),s.jsx(t,{}),s.jsx(a,{})]});export{x as default};
---
+++ dist/assets/main-layout-Dc3lxKfr.js
@@ -0,0 +1,1 @@
+import{j as s,O as t}from"./index-bBvgA6QN.js";import{H as r,F as a}from"./header-DYzC6E-o.js";const x=()=>s.jsxs(s.Fragment,{children:[s.jsx(r,{}),s.jsx(t,{}),s.jsx(a,{})]});export{x as default};
