1e531203551986ded77d305485c21726ecc23cf2
student <rabinyaupane55@gmail.com>
2025-01-28 23:06:29 +0545

    Place order UI completed

---
+++ dist/assets/main-layout-BQx0PTFc.js
@@ -0,0 +1,1 @@
+import{j as s,O as t}from"./index-Cuo50Cr3.js";import{H as r,F as a}from"./header-BKDHgIF-.js";const x=()=>s.jsxs(s.Fragment,{children:[s.jsx(r,{}),s.jsx(t,{}),s.jsx(a,{})]});export{x as default};
